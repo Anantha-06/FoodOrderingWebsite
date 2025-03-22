bb851442dac20a7ac121ebb059f3725be574fa43
Manu0601 <manu06011998@gmail.com>
2025-03-22 07:20:23 +0530

    render update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173", 
-  "https://food-ordering-website-git-main-anantha-krishnan-ms-projects.vercel.app" // 
+  "https://food-ordering-website-wine.vercel.app/" 
 ];
 
 app.use(
