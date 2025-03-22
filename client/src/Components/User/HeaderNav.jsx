e222ec440b17ea928cb8d39436c14520dc80fc9c
Manu0601 <manu06011998@gmail.com>
2025-03-22 07:55:13 +0530

    v10

--- client/src/Components/User/HeaderNav.jsx
+++ client/src/Components/User/HeaderNav.jsx
@@ -8,7 +8,6 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import "../../App.css";
 import { Link, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import { motion } from "framer-motion";
 
 function HeaderNav() {
   const navigate = useNavigate();
@@ -20,18 +19,18 @@ function HeaderNav() {
   };
 
   return (
-    <motion.nav initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }}>
+    <nav>
       <Navbar expand="lg" className="bg-warning">
         <Container fluid className="gap-lg-5 gap-md-3 gap-sm-1 gap-xs-0">
           <Link to="/">
-            <motion.div whileHover={{ scale: 1.1 }}>
+            <div>
               <Navbar.Brand>
                 <img
                   src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1740756873/Food%20Order%20Website/Byteeats%20Profile%20Logo.png"
                   className="navBar-logimg"
                 />
               </Navbar.Brand>
-            </motion.div>
+            </div>
           </Link>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
@@ -40,36 +39,28 @@ function HeaderNav() {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <motion.div whileHover={{ scale: 1.1 }}>
-                <Nav.Link href="/">Home</Nav.Link>
-              </motion.div>
-              <motion.div whileHover={{ scale: 1.1 }}>
-                <Nav.Link href="/about">About Us</Nav.Link>
-              </motion.div>
-              <motion.div whileHover={{ scale: 1.1 }}>
-                <NavDropdown
-                  title={
-                    <img
-                      className="thumbnail-image"
-                      src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1740756875/Food%20Order%20Website/noeuwugmxrhszkjcq2no.png"
-                    />
-                  }
-                  id="navbarScrollingDropdown"
-                >
-                  <NavDropdown.Item href="/profile">Your Profile</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Your Order</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item onClick={handleSignOut}>Sign Out</NavDropdown.Item>
-                </NavDropdown>
-              </motion.div>
-              <motion.div whileHover={{ scale: 1.1 }}>
-                <Nav.Link href="/checkoutpage">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About Us</Nav.Link>
+              <NavDropdown
+                title={
                   <img
-                    src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1740756875/Food%20Order%20Website/hj0p5muuflnvmef2lq9z.png"
-                    className="thumbnail-image-2"
+                    className="thumbnail-image"
+                    src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1740756875/Food%20Order%20Website/noeuwugmxrhszkjcq2no.png"
                   />
-                </Nav.Link>
-              </motion.div>
+                }
+                id="navbarScrollingDropdown"
+              >
+                <NavDropdown.Item href="/profile">Your Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Your Order</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={handleSignOut}>Sign Out</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/checkoutpage">
+                <img
+                  src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1740756875/Food%20Order%20Website/hj0p5muuflnvmef2lq9z.png"
+                  className="thumbnail-image-2"
+                />
+              </Nav.Link>
             </Nav>
             <Form className="d-flex">
               <Form.Control
@@ -78,14 +69,12 @@ function HeaderNav() {
                 className="me-2"
                 aria-label="Search"
               />
-              <motion.div whileTap={{ scale: 0.9 }}>
-                <Button variant="outline-success">Search</Button>
-              </motion.div>
+              <Button variant="outline-success">Search</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </motion.nav>
+    </nav>
   );
 }
 
