b0147927dc1010ff54fbb7ea1459155049421036
Manu0601 <manu06011998@gmail.com>
2025-03-22 07:27:44 +0530

    V4

--- server/server.js
+++ server/server.js
@@ -5,13 +5,15 @@ import cors from "cors";
 import { apiRouter } from "./routes/version_1/index.js";
 import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://food-ordering-website-wine.vercel.app/" 
+  "http://localhost:5173",
+  "https://byteeats.vercel.app/login",
+  "https://byteeats-anantha-krishnan-ms-projects.vercel.app/login",
+  "https://byteeats-git-main-anantha-krishnan-ms-projects.vercel.app/login",
 ];
 
 app.use(
@@ -23,8 +25,8 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
