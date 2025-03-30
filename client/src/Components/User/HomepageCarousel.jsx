17fdd16561fdd3914b97d36cb62f18ac240792e6
Manu0601 <manu06011998@gmail.com>
2025-03-30 20:16:24 +0530

    v18 homepage

--- client/src/Components/User/HomepageCarousel.jsx
+++ client/src/Components/User/HomepageCarousel.jsx
@@ -7,23 +7,37 @@ function HomepageCarousel() {
     <Carousel>
       <Carousel.Item interval={1000}>
         <img
-          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1742383616/Food%20Order%20Website/qjusdvpze3heuim4svg3.jpg"
+          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1743344757/3015488-hd_1920_1080_24fps_bxngj2.gif"
           className="carousel-image"
           alt="Slider Image 1"
         />
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <img
-          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1742383616/Food%20Order%20Website/jhvy6ffy9hr2lex4hrnu.jpg"
+          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1743344884/3195347-uhd_3840_2160_25fps_fpmiqw.gif"
           className="carousel-image"
           alt="Slider Image 2"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1742383617/Food%20Order%20Website/jefiatfmwtyvzcivxfha.jpg"
+          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1743344617/1908426-uhd_3840_2160_25fps_jekqvz.gif"
           className="carousel-image"
           alt="Slider Image 3"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1743345703/Untitled_design_1_jek4od.gif"
+          className="carousel-image"
+          alt="Slider Image 4"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          src="https://res.cloudinary.com/dzmymp0yf/image/upload/v1743345857/Untitled_design_2_aybhb8.gif"
+          className="carousel-image"
+          alt="Slider Image 5"
         />
       </Carousel.Item>
     </Carousel>
