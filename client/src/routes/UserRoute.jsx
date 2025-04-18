f28d4c060254cbefe9e7eae6cbdcf96fd8df52c3
Manu0601 <manu06011998@gmail.com>
2025-04-18 15:21:18 +0530

    v18 website css update

--- client/src/routes/UserRoute.jsx
+++ client/src/routes/UserRoute.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import HeaderNav from "../Components/User/HeaderNav";
 import FooterNav from "../Components/User/FooterNav";
 import { Outlet } from "react-router-dom";
 
 function UserRoute() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+   
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
-    <>
-      <div id="root">
-        <HeaderNav />
-        <div className="page-content">
-          <Outlet />
-        </div>
-        <FooterNav />
-      </div>
-    </>
+    <div id="root" className="scroll-container">
+      <HeaderNav />
+      <main className="page-content">
+        <Outlet />
+      </main>
+      <FooterNav />
+    </div>
   );
 }
 
